cbda1f72f35fd9169c658c74451b7885493a8253
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-05-26 13:06:58 -0400

    Update QRCodeLabel.tsx

--- app/components/QRCodeLabel.tsx
+++ app/components/QRCodeLabel.tsx
@@ -1,6 +1,8 @@
 import QRCode from 'react-qr-code';
 import React from 'react';
+import localFont from 'next/font/local';
 
+const myFont = localFont({ src: '../../public/gill-sans-alt-one-light.otf' });
 interface FabricRow {
 	Fabric: string;
 	Color: string;
@@ -16,9 +18,9 @@ const QRCodeLabel: React.FC<QRCodeLabelProps> = ({ fabric }) => (
 	<div className='label w-[2.625in] h-[1in] flex flex-row items-center justify-center text-[9pt] text-center overflow-hidden'>
 		<QRCode fgColor='#f58625' value={fabric.URL} size={56} />
 		<div className='ml-2 truncate text-left'>
-			<p className="uppercase">{fabric.Fabric},</p>
+			<p className="uppercase myFont.className">{fabric.Fabric},</p>
 			<strong className="capitalize">{fabric.Color}</strong>
-			<p>Multi-Use</p>
+			<p className="myFont.className">Multi-Use</p>
 		</div>
 	</div>
 );
