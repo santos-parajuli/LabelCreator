cfff60487eec8ebdbfe5daa6336dc5eff7e5e8f8
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-05-26 10:10:25 -0400

    Update LabelGrid.tsx

--- app/components/LabelGrid.tsx
+++ app/components/LabelGrid.tsx
@@ -14,7 +14,7 @@ type LabelGridProps = {
 };
 
 const LabelGrid: React.FC<LabelGridProps> = ({ fabric, count = 30 }) => (
-	<div className='grid grid-cols-3 grid-rows-10 p-[0.5in] w-[8.5in] mx-auto' style={{ pageBreakInside: 'avoid' }}>
+	<div className='grid grid-cols-3 grid-rows-10 py-[0.5in] px-[0.1875in] w-[8.5in] mx-auto' style={{ pageBreakInside: 'avoid' }}>
 		{Array.from({ length: count }).map((_, idx) => (
 			<QRCodeLabel key={idx} fabric={fabric} />
 		))}
