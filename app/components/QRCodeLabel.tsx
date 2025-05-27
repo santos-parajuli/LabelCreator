cda55582c50bb56584d3d744b37a75653d0be7af
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-05-27 11:22:40 -0400

    Update QRCodeLabel.tsx

--- app/components/QRCodeLabel.tsx
+++ app/components/QRCodeLabel.tsx
@@ -3,24 +3,28 @@ import React from 'react';
 import localFont from 'next/font/local';
 
 const myFont = localFont({ src: '../../public/gill-sans-alt-one-light.otf' });
-interface FabricRow {
+
+export interface FabricRow {
 	Fabric: string;
 	Color: string;
 	'On Sale': string;
 	URL: string;
 }
 
+export type LabelType = 'Multi-Use' | 'High Performance' | 'High Performance/Outdoor' | 'Drapery';
+
 type QRCodeLabelProps = {
 	fabric: FabricRow;
+	labelType: LabelType;
 };
 
-const QRCodeLabel: React.FC<QRCodeLabelProps> = ({ fabric }) => (
+const QRCodeLabel: React.FC<QRCodeLabelProps> = ({ fabric, labelType }) => (
 	<div className='label w-[2.625in] h-[1in] flex flex-row items-center justify-center text-[10pt] text-center overflow-hidden'>
 		<QRCode fgColor='#000000' value={fabric.URL} size={56} />
 		<div className='ml-3 truncate text-left'>
 			<p className={`uppercase ${myFont.className}`}>{fabric.Fabric},</p>
 			<p className="capitalize font-bold font-[arial]">{fabric.Color}</p>
-			<p className={myFont.className}>Multi-Use</p>
+			<p className={myFont.className}>{labelType}</p>
 		</div>
 	</div>
 );
