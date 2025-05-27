ea8bd0f49f31443ba1951c3c5e71e88b14b2909c
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-05-27 11:22:54 -0400

    Update SingleQRCodePreview.tsx

--- app/components/SingleQRCodePreview.tsx
+++ app/components/SingleQRCodePreview.tsx
@@ -1,20 +1,16 @@
-import QRCodeLabel from './QRCodeLabel'; // 👈 reuse the same label
-import React from 'react';
+'use client';
 
-interface FabricRow {
-	Fabric: string;
-	Color: string;
-	'On Sale': string;
-	URL: string;
-}
+import React from 'react';
+import QRCodeLabel, { FabricRow, LabelType } from './QRCodeLabel';
 
 type SingleQRCodePreviewProps = {
 	fabric: FabricRow;
+	labelType: LabelType;
 };
 
-const SingleQRCodePreview: React.FC<SingleQRCodePreviewProps> = ({ fabric }) => (
+const SingleQRCodePreview: React.FC<SingleQRCodePreviewProps> = ({ fabric, labelType }) => (
 	<div className='mb-6 flex justify-center print:hidden'>
-		<QRCodeLabel fabric={fabric} />
+		<QRCodeLabel fabric={fabric} labelType={labelType} />
 	</div>
 );
 
