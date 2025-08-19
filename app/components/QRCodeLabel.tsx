5835460ee318fd2620b075aca7eec5d0697f4227
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2025-08-19 13:09:57 -0400

    Update QRCodeLabel.tsx

--- app/components/QRCodeLabel.tsx
+++ app/components/QRCodeLabel.tsx
@@ -5,34 +5,39 @@ import localFont from 'next/font/local';
 const myFont = localFont({ src: '../../public/gill-sans-alt-one-light.otf' });
 
 export interface FabricRow {
-	Fabric: string;
-	Color: string;
-	'On Sale': string; // 'Yes' or 'No'
-	URL: string;
+  Fabric: string;
+  Color: string;
+  'On Sale': string; // 'Yes' or 'No'
+  URL: string;
 }
 
-export type LabelType = 'Multi-Use' | 'High Performance' | 'High Performance/Outdoor' | 'Drapery' | 'Multi-Use, Pillow Only' | 'Double Width, Drapery';
+export type LabelType =
+  | 'Multi-Use'
+  | 'High Performance'
+  | 'High Performance/Outdoor'
+  | 'Drapery'
+  | 'Multi-Use, Pillow Only'
+  | 'Double Width, Drapery';
 
 type QRCodeLabelProps = {
-	fabric: FabricRow;
-	labelType: LabelType;
+  fabric: FabricRow;
+  labelType: LabelType;
 };
 
 const QRCodeLabel: React.FC<QRCodeLabelProps> = ({ fabric, labelType }) => (
-	<div className='label w-[2.625in] h-[1in] flex flex-row items-center justify-center text-[9pt] text-center overflow-hidden'>
-		<QRCode fgColor='#000000' value={fabric.URL} size={56} />
-		<div className='ml-3 truncate text-left'>
-			<p className={`uppercase ${myFont.className}`}>
-			<p className={`uppercase ${myFont.className}`}>
-				{fabric.Fabric?.trim() ? `${fabric.Fabric},` : ""}
-			</p>
-			<p className="capitalize font-bold font-[arial]">{fabric.Color}</p>
-			<p className={myFont.className}>{labelType}</p>
-			{fabric['On Sale'].toLowerCase() === 'yes' && (
-				<p className={`${myFont.className} text-orange-500 font-bold`}>Sale</p>
-			)}
-		</div>
-	</div>
+  <div className="label w-[2.625in] h-[1in] flex flex-row items-center justify-center text-[9pt] text-center overflow-hidden">
+    <QRCode fgColor="#000000" value={fabric.URL} size={56} />
+    <div className="ml-3 truncate text-left">
+      <p className={`uppercase ${myFont.className}`}>
+        {fabric.Fabric?.trim() ? `${fabric.Fabric},` : ""}
+      </p>
+      <p className="capitalize font-bold font-[arial]">{fabric.Color}</p>
+      <p className={myFont.className}>{labelType}</p>
+      {String(fabric['On Sale'] || '').toLowerCase() === 'yes' && (
+        <p className={`${myFont.className} text-orange-500 font-bold`}>Sale</p>
+      )}
+    </div>
+  </div>
 );
 
 export default QRCodeLabel;
